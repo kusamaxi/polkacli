562a5c91de0a95f9fb6c2ffbc95d6e3d1116599b
user <contact@expectchaos.com>
2023-02-25 07:18:36 +0200

    chajnges

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ cp config ~/.config/polkacli
 
 # Copy the polkacli completion script to the config directory
 cp completion ~/.config/polkacli
+# Add the completion script to the bashrc/zshrc file if it doesn't already exist
+grep -qxF 'source ~/.config/polkacli/completion' ~/.bashrc || echo "source ~/.config/polkacli/completion" >> ~/.bashrc
+grep -qxF 'source ~/.config/polkacli/completion' ~/.zshrc || echo "source ~/.config/polkacli/completion" >> ~/.zshrc
 
 # Copy the polkacli logo file to the config directory
 cp logo.ansi ~/.config/polkacli
